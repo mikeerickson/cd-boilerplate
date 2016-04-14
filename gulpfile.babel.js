e838ac016342cbbe9388f8476f12ffd75005b29f
Mike Erickson <codedungeon@gmail.com>
2016-04-13 20:29:52 -0700

    time to push

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,9 @@
-// GULPFILE
+
+import requireDir from 'require-dir'
+
+// PRELOAD ALL TASKS
 // =============================================================================
+// you can execute individual tasks as `gulp <taskName>`
+// WARNING: don't load recursively (omit `_disabled` tasks)
 
-/*global require*/
-
+requireDir('./tasks', { recurse: false });
---
+++ tasks/gulp.config.js
@@ -0,0 +1,62 @@
+// TASK CONFIGURATION
+// task configuration only, use ./config for application configuration
+
+module.exports = {
+
+	defaults: {
+		show: true
+	},
+
+	copy: {
+		assets: [
+			{src: './node_modules/bootstrap/dist/css/bootstrap.min.css', dest: './dist/vendor/bootstrap.min.css'},
+			{src: './node_modules/bootstrap/fonts/', dest: './dist/fonts'}
+		],
+		html: {
+			src: './src/**/*.html',
+			dest: './dist'
+		}
+	},
+
+	scripts: {
+		src:      ['.src/js/**/*.js','./src/js/**/*.jsx','!.src/js/**/*rt.js'],
+		entry:    './src/js/index.jsx',
+		dest:     './dist/js',
+		filename: 'app.js'
+	},
+
+	lint: {
+		src:  ['src/js/**/*.js','tasks/**/*.js','specs/**/*Specs.js','!.src/js/**/*rt.js']
+	},
+
+	less: {
+		src: './src/less/**/*.less',
+		dist: './dist/css',
+		filename: 'app.css'
+	},
+
+	specs: {
+		src: ['specs/**/*Spec.js'],
+		openReport: false
+	},
+
+	clean: {
+		src: ['logs', 'specs/logs','TODO.md','todo.json'],
+		dest: './dist'
+	},
+
+	templates: {
+		src: ['./src/js/templates/**/*.rt'],
+		dest: './src/js/templates/'
+	},
+
+	todo: {
+		src: [
+			'./src/js/**/*.js',
+			'./specs/**/*.js',
+			'./tasks/**/*.js'
+		],
+		filename: 'TODO.md'
+	}
+
+};
